e7135286401228ce506926615411eb6d43babfe1
ChoiSittichai <emotionctrl@gmail.com>
2025-03-30 16:28:35 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,10 @@
-// App.jsx - placeholder
+function App() {
+  return (
+    <div>
+      <h1>Hello ScriptForge!</h1>
+      <p>This is your first deployed app.</p>
+    </div>
+  );
+}
+
+export default App;
