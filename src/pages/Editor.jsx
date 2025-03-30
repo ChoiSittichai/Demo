1c284befd4048eb9f02a4ed397c9a62badb8ed7c
ChoiSittichai <emotionctrl@gmail.com>
2025-03-30 17:51:01 +0700

    Update Editor.jsx

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,1 +1,10 @@
-// Editor.jsx - placeholder
+function Editor() {
+  return (
+    <div>
+      <h2>Editor Section</h2>
+      <p>This is where your script editor will go.</p>
+    </div>
+  );
+}
+
+export default Editor;
