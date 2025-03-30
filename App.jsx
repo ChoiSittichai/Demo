c5d63f3e269a206f0961f3e1f1bb1e8449998e09
ChoiSittichai <emotionctrl@gmail.com>
2025-03-30 17:50:07 +0700

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,1 +1,14 @@
-// App.jsx - placeholder
+import Editor from './pages/Editor';
+import Dashboard from './pages/Dashboard';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello ScriptForge!</h1>
+      <Editor />
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
