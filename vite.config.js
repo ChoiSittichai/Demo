e8b3d858da98affb828ba0877f5c093f070ca48a
ChoiSittichai <emotionctrl@gmail.com>
2025-03-31 17:54:42 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    host: true,
+    open: true
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 })
